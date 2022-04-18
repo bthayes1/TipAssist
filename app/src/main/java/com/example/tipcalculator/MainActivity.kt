fee5dd58ebee2e44b1e1352330810b0e2961d8af
bthayes1 <79590455+bthayes1@users.noreply.github.com>
2022-04-18 16:38:29 -0400

    First Version of the Tip Calculator

--- app/src/main/java/com/example/tipcalculator/MainActivity.kt
+++ app/src/main/java/com/example/tipcalculator/MainActivity.kt
@@ -1,11 +1,79 @@
 package com.example.tipcalculator
 
+import android.annotation.SuppressLint
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
+import android.widget.EditText
+import android.widget.SeekBar
+import android.widget.SeekBar.OnSeekBarChangeListener
+import android.widget.TextView
+
+private const val TAG = "MainActivity"
+private const val INIT_TIP_PERCENT = 15
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var tvTipPercent: TextView
+    private lateinit var tipScroll: SeekBar
+    private lateinit var tvTipAmount: TextView
+    private lateinit var tvTotalAmount: TextView
+    private lateinit var billAmount: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        tvTipPercent = findViewById(R.id.tvTipPercent)
+        tipScroll = findViewById(R.id.tipPercentBar)
+        tvTipAmount = findViewById(R.id.tvTipAmount)
+        tvTotalAmount = findViewById(R.id.tvTotal)
+        billAmount = findViewById(R.id.tvAmountInput)
+
+        //Initialize tip value at 15%
+        tipScroll.progress = INIT_TIP_PERCENT
+        tvTipPercent.text = INIT_TIP_PERCENT.toString()
+
+
+        tipScroll.setOnSeekBarChangeListener(object : OnSeekBarChangeListener {
+            @SuppressLint("SetTextI18n")
+            override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
+                //When change occurs on scrollbar, info is logged, and values are calculated
+                Log.i(TAG, "Current Progress is: $p1")
+                tvTipPercent.text = "$p1%"
+                tipAndTotalCalc()
+            }
+            //No logic needed for these functions
+            override fun onStartTrackingTouch(p0: SeekBar?) {}
+            override fun onStopTrackingTouch(p0: SeekBar?) {}
+        })
+        billAmount.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+            override fun afterTextChanged(p0: Editable?) {
+                //Changes on edit text are logged and values are calculated
+                Log.i(TAG, "Current Input is: $p0")
+                tipAndTotalCalc()
+            }
+        })
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun tipAndTotalCalc() {
+        //If there is no characters, return
+        if (billAmount.text.isEmpty()) {
+            tvTipAmount.text = ""
+            tvTotalAmount.text = ""
+            return
+        }
+        //Calculate total and tip amounts
+        val billAmt = billAmount.text.toString().toDouble()
+        val tipPercent = tipScroll.progress
+        val tipAmt = billAmt *  tipPercent/ 100
+        val total = tipAmt + billAmt
+        //Update UI
+        tvTotalAmount.text = "%.2f".format(total)
+        tvTipAmount.text = "%.2f".format(tipAmt)
     }
 }
